1bc3ec514f63384ba04434d7e5796fd027d5cddd
Kyle So <kyle.p.so@gmail.com>
2019-02-12 15:38:22 -0800

    fix routing issues for / and clucks/ and sign_in

--- routes/baseRouter.js
+++ routes/baseRouter.js
@@ -1,21 +1,30 @@
-const express = require('express');
+const express = require("express");
+const knex = require("../db/client");
 const router = express.Router();
 
 const COOKIE_MAX_AGE = 1000 * 60 * 60 * 24 * 7;
 
-router.get('/', (req, res) => {
-  res.render('sign_in');
+router.get("/", (req, res) => {
+  knex("clucks")
+    .orderBy("created_at", "desc")
+    .then(clucks => {
+      res.render("clucks/index", { clucks: clucks });
+    });
 });
 
-router.post('/sign_in', (req, res) => {
-	const params = req.body;
-	res.cookie('username', params.username, { maxAge: COOKIE_MAX_AGE });
-	res.redirect('/clucks');
+router.get("/sign_in", (req, res) => {
+  res.render("sign_in");
 });
 
-router.post('/sign_out', (req, res) => {
-	res.clearCookie('username');
-	res.redirect('/');
+router.post("/sign_in", (req, res) => {
+  const params = req.body;
+  res.cookie("username", params.username, { maxAge: COOKIE_MAX_AGE });
+  res.redirect("/clucks");
+});
+
+router.post("/sign_out", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/");
 });
 
 module.exports = router;
